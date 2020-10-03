765a03b00af10416ff106dc90bd196ef98ef2f5e
Actions <milo5huntington@gmail.com>
2020-10-03 08:04:49 +0000

    Linted

--- src/client/Client.ts
+++ src/client/Client.ts
@@ -1,34 +1,32 @@
-import { Client } from "discord.js";
-import { ClientOptions, Dezu } from "../interfaces/Interfaces";
-import defaults from "lodash.defaults";
-import Logger from "../modules/Logger";
+import { Client } from 'discord.js';
+import { ClientOptions, Dezu } from '../interfaces/Interfaces';
+import defaults from 'lodash.defaults';
+import Logger from '../modules/Logger';
 class DezuClient extends Client {
-  private dezu: Dezu = {};
-  public constructor(options: ClientOptions) {
-    super(options.clientOptions ? options.clientOptions : {});
-    this.dezu.options = defaults(options, {
-      owners: [],
-      clientOptions: {},
-      useDezuLogger: true,
-    });
-    this.dezu.logger = new Logger(this.dezu.options.useDezuLogger);
-  }
-  public start(): void {
-    this.login(this.dezu.options.token)
-      .then(() => this.dezu.logger.info(`Logging in...`))
-      .catch((reason) => {
-        this.dezu.logger.error(reason.message);
-        process.exit(1);
-      });
-    this.on("ready", () =>
-      this.dezu.logger.info(`${this.user.tag} is online.`)
-    );
-  }
+	private dezu: Dezu = {};
+	public constructor(options: ClientOptions) {
+		super(options.clientOptions ? options.clientOptions : {});
+		this.dezu.options = defaults(options, {
+			owners: [],
+			clientOptions: {},
+			useDezuLogger: true,
+		});
+		this.dezu.logger = new Logger(this.dezu.options.useDezuLogger);
+	}
+	public start(): void {
+		this.login(this.dezu.options.token)
+			.then(() => this.dezu.logger.info(`Logging in...`))
+			.catch((reason) => {
+				this.dezu.logger.error(reason.message);
+				process.exit(1);
+			});
+		this.on('ready', () => this.dezu.logger.info(`${this.user.tag} is online.`));
+	}
 }
 export default DezuClient;
 new DezuClient({
-  commandHandler: { path: "" },
-  token: "",
-  prefix: "",
-  useDezuLogger: true,
+	commandHandler: { path: '' },
+	token: '',
+	prefix: '',
+	useDezuLogger: true,
 }).start();
--- src/modules/Logger.ts
+++ src/modules/Logger.ts
@@ -1,26 +1,26 @@
-import chalk from "chalk";
-import logSymbols from "log-symbols";
+import chalk from 'chalk';
+import logSymbols from 'log-symbols';
 class Logger {
-  private _active: boolean = false;
-  private chalk: chalk.Chalk = chalk.bold.underline.italic;
-  public constructor(active: boolean) {
-    this._active = active;
-  }
-  public success(message: any): boolean {
-    if (!this._active) return false;
-    console.log(`${logSymbols.success} ${this.chalk.green(message)}`);
-  }
-  public info(message: any): boolean {
-    if (!this._active) return false;
-    console.log(`${logSymbols.info} ${this.chalk.blue(message)}`);
-  }
-  public error(message: any): boolean {
-    if (!this._active) return false;
-    console.log(`${logSymbols.error} ${this.chalk.red(message)}`);
-  }
-  public warn(message: any): boolean {
-    if (!this._active) return false;
-    console.log(`${logSymbols.warning} ${this.chalk.yellow(message)}`);
-  }
+	private _active: boolean = false;
+	private chalk: chalk.Chalk = chalk.bold.underline.italic;
+	public constructor(active: boolean) {
+		this._active = active;
+	}
+	public success(message: any): boolean {
+		if (!this._active) return false;
+		console.log(`${logSymbols.success} ${this.chalk.green(message)}`);
+	}
+	public info(message: any): boolean {
+		if (!this._active) return false;
+		console.log(`${logSymbols.info} ${this.chalk.blue(message)}`);
+	}
+	public error(message: any): boolean {
+		if (!this._active) return false;
+		console.log(`${logSymbols.error} ${this.chalk.red(message)}`);
+	}
+	public warn(message: any): boolean {
+		if (!this._active) return false;
+		console.log(`${logSymbols.warning} ${this.chalk.yellow(message)}`);
+	}
 }
 export default Logger;
