823d9cd4906e76de061ec5cc4e22b8941cad01bb
Milo123459 <milo5huntington@gmail.com>
2020-10-03 09:02:43 +0100

    add logger and ci

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # discord-dezu
+
 A fast and efficient wrapper for discord.js. Written in TypeScript.
--- src/client/Client.ts
+++ src/client/Client.ts
@@ -1,14 +1,34 @@
 import { Client } from "discord.js";
-import { ClientOptions, ClientDezu } from "../interfaces/Interfaces";
+import { ClientOptions, Dezu } from "../interfaces/Interfaces";
 import defaults from "lodash.defaults";
+import Logger from "../modules/Logger";
 class DezuClient extends Client {
-    private dezu: ClientDezu;
-    public constructor (options: ClientOptions) {
-        super(options.clientOptions ? options.clientOptions : {});
-        this.dezu.options = defaults(options, { owners: [], clientOptions: {}, useDezuLogger: true });
-    };
-    public start(): void {
-        
-    }
-};
+  private dezu: Dezu = {};
+  public constructor(options: ClientOptions) {
+    super(options.clientOptions ? options.clientOptions : {});
+    this.dezu.options = defaults(options, {
+      owners: [],
+      clientOptions: {},
+      useDezuLogger: true,
+    });
+    this.dezu.logger = new Logger(this.dezu.options.useDezuLogger);
+  }
+  public start(): void {
+    this.login(this.dezu.options.token)
+      .then(() => this.dezu.logger.info(`Logging in...`))
+      .catch((reason) => {
+        this.dezu.logger.error(reason.message);
+        process.exit(1);
+      });
+    this.on("ready", () =>
+      this.dezu.logger.info(`${this.user.tag} is online.`)
+    );
+  }
+}
 export default DezuClient;
+new DezuClient({
+  commandHandler: { path: "" },
+  token: "",
+  prefix: "",
+  useDezuLogger: true,
+}).start();
